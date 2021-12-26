576ae04ffabaca9af8a716fcca9ee13dd1ee6457
Murod Akhmedov <murod.london@gmail.com>
2021-12-26 15:31:30 +0000

    build the testimonial

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".mySwiper", {
+  // cssMode: true,
+  speed: 750,
+  parallax: true,
+  navigation: {
+    nextEl: ".fa-chevron-right",
+    prevEl: ".fa-chevron-left",
+  },
+
+  mousewheel: true,
+  keyboard: true,
+});
