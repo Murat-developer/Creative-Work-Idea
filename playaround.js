d82f31fd97d547cd81e446749d778bf5c0f05d6c
Murod Akhmedov <murod.london@gmail.com>
2022-01-02 15:54:18 +0000

    build the plan block

--- playaround.js
+++ playaround.js
@@ -1,20 +1,11 @@
-const hamburger = document.querySelector(".hamburger");
-hamburger.addEventListener("click", function () {
-  this.classList.toggle("is-active");
-});
-
-const navSlide = () => {
-  const hamburger = document.querySelector(".hamburger");
-  const nav = document.querySelector(".list");
-  hamburger.addEventListener("click", () => {
-    nav.classList.toggle("nav-active");
-  });
-};
-navSlide();
-
-const collapsibles = document.querySelectorAll(".collapsible");
-collapsibles.forEach((item) =>
-  item.addEventListener("click", function () {
-    this.classList.toggle("collapsible--expanded");
-  })
-);
+// let userName = prompt("Your Name");
+// let userSurname = prompt("Your Surname");
+// let userAvto = prompt("Auto number");
+// const korp = "100$";
+// alert(`${userName} ${userSurname} You've got a bill  ${korp}`);
+// let payMent = confirm("Woul you like to pay your bill?");
+// let transLation = prompt("How much would you like to pay?");
+// let yesNo = confirm("Please confirm your payment");
+// alert(
+//   `${userName} ${userSurname} You've paid ${transLation}.receive 567849309503`
+// );
